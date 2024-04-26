541d2541661cc0a03291bd88cb7adfeaf6c1c756
doleminhtriet <doleminhtriet@csu.fulllerton.edu>
2024-04-26 11:20:20 -0700

    init

--- action.php
+++ action.php
@@ -261,10 +261,10 @@
 	if (isset($_POST["count_item"])) {
 		//When user is logged in then we will count number of item in cart by using user session id
 		if (isset($_SESSION["uid"])) {
-			$sql = "SELECT COUNT(*) AS count_item FROM cart WHERE user_id = $_SESSION[uid]";
+			$sql = "SELECT COUNT(*) AS count_item FROM Cart WHERE UserID = $_SESSION[uid]";
 		}else{
 			//When user is not logged in then we will count number of item in cart by using users unique ip address
-			$sql = "SELECT COUNT(*) AS count_item FROM cart WHERE ip_add = '$ip_add' AND user_id < 0";
+			$sql = "SELECT COUNT(*) AS count_item FROM Cart WHERE IpAdd = '$ip_add' AND UserID < 0";
 		}
 		
 		$query = mysqli_query($con,$sql);
